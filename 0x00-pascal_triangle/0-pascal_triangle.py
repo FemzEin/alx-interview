802e86909c3f3b04be398d17bf6f31b5a3a968e5
Albert James <femzein@gmail.com>
2023-07-01 20:11:56 -0700

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,23 +8,23 @@ def pascal_triangle(n):
 
     if n <= 0:
         yield from ()
-# Initialize the first row
+""" Initialize the first row """
     row = [1]
-# Yield the first row
+""" Yield the first row """
     yield row
 
 
-# Loop from 1 to n-1
+""" Loop from 1 to n-1 """
 for i in range(1, n):
     next_row = [1]
-# Loop from 1 to i-1
+""" Loop from 1 to i-1 """
 for j in range(1, i):
     sum = row[j] + row[j-1]
-# Append the sum to the next row
+""" Append the sum to the next row """
     next_row.append(sum)
-# Append the last element to the next row
+""" Append the last element to the next row """
     next_row.append(1)
-# Yield the next row
+""" Yield the next row """
 yield next_row
-# Update the current row with the next row
+""" Update the current row with the next row """
 row = next_row
